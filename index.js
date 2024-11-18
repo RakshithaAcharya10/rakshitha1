f16c68b7f45dd9dfee85d651e38a1b3e636e676e
Rakshitha Acharya <rakshitha@example.com>
2024-11-18 23:03:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//console.log(`hello`);
+//console.log(`I like dancing`);
+
+//window.alert(`This is an alert`);
+
+document.getElementById(`myH1`).textContent=`Hello`;
+document.getElementById(`myP`).textContent=`I like cooking`;
